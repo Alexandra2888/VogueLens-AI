9442efacf69415be13920b3c9d48e7d7f5136cf3
Alex <moldovan.alexandra28@gmail.com>
2026-03-23 17:40:41 +0200

    Add Supabase storage configuration to Next.js settings; update Content Security Policy to allow connections to Supabase resources.

--- next.config.mjs
+++ next.config.mjs
@@ -26,6 +26,11 @@ const nextConfig = {
         hostname: 'res.cloudinary.com',
         pathname: '/dblgunawk/**',
       },
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
     ],
   },
 
@@ -67,7 +72,7 @@ const nextConfig = {
               "img-src 'self' data: blob: https:",
               "font-src 'self' data: https://client.crisp.chat",
               "worker-src blob: 'self'",
-              "connect-src 'self' https://api.openai.com https://*.clerk.com https://*.clerk.accounts.dev https://accounts.google.com wss://*.clerk.com https://*.crisp.chat wss://relay.crisp.chat https://va.vercel-scripts.com https://vitals.vercel-insights.com",
+              "connect-src 'self' https://api.openai.com https://*.clerk.com https://*.clerk.accounts.dev https://accounts.google.com wss://*.clerk.com https://*.crisp.chat wss://relay.crisp.chat https://va.vercel-scripts.com https://vitals.vercel-insights.com https://*.supabase.co",
               'frame-src https://client.crisp.chat https://game.crisp.chat',
               "frame-ancestors 'none'",
               "object-src 'none'",
